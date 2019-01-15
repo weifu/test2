1d7aaa01be8d5f27072182424eb322c59b6ed3f7
weifu_tseng <weifu_tseng@askey.com.tw>
2019-01-15 17:27:44 +0800

    dddd[#0011]: adsdfsdf
    Description:
    Affect Area: (UI behavior/Wifi/Phone/NFC/CTS/...)
    Fix Type: (Bug fixing/Enhancement/feature/Others)
    Root Cause:
    
    Change Description:
    
    ReviewBoard link:

---
+++ ddxx.cpp
@@ -0,0 +1,24 @@
+/**
+ ** copyright(C)2016 Kicad Developers, see (a.a) for contributors.
+ *** copyright
+ ** copyright(C) 2019
+ **/
+#  * @copyright   Copyright (c) 2013, 2015 Holder
+# All rights reserved.
+
+
+# Copyright 2002,2012-2014 Holder
+#      Was: 2002,2012-2014 Holder
+#
+# Copyright (c) 2002,2012-2014 Holder
+#      Was: (c) 2002,2012-2014 Holder
+#
+# COPYRIGHT &copy; 2002,2012-2014 Holder
+#      Was: &copy; 2002,2012-2014 Holder
+#
+# Copyright © 2002,2012-2014 Holder2001 and 2002
+#      Was: © 2002,2012-2014 Holder2001 and 2002
+#
+oooo
+jkllllllhjklh
+xxxxx:w
